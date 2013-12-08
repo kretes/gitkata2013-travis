458ba0fb136f80a67fc1aea2aab66c1345f838a6
Tomasz Bartczak <kretes@gazeta.pl>
2013-12-08 12:35:20 +0100

    some pythogn tests

---
+++ test_something.py
@@ -0,0 +1,10 @@
+import unittest
+
+class GitkataTest(unittest.TestCase):
+
+    def test_something(self):
+        self.assertEqual(2, 1+1)
+        self.assertEqual(4, 2+2)
+        self.assertEqual(6, 3+3)
+        self.assertEqual(8, 4+4)
+        self.assertEqual(100, 99+1)
